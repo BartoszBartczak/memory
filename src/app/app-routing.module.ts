cfe2572ff1fe65d42a10b24fdaa45e98396dea75
BartoszBartczak <intermarism@gmail.com>
2021-09-19 18:31:00 +0200

    New components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './views/about-us/about-us.component';
+import { BuyComponent } from './views/buy/buy.component';
 import { MainComponent } from './views/main/main.component';
  
 const routes: Routes = [
    { path: 'main', component: MainComponent },
    { path: '', redirectTo: '/main', pathMatch: 'full'  },
+   { path: 'about-us', component: AboutUsComponent },
+   { path: 'buy', component: BuyComponent },
+
 
 ];
 
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,16 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { BuyComponent } from './buy/buy.component';
 
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    AboutUsComponent,
+    BuyComponent
   ],
   imports: [
     CommonModule,
     RouterModule
+  ],
+
+  exports: [
+    BuyComponent,
+    AboutUsComponent,
+    MainComponent
   ]
 })
 export class ViewsModule { }
