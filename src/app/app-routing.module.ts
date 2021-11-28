04ceb0796685b8d92932666abdaf754f56243c20
BartoszBartczak <intermarism@gmail.com>
2021-11-28 17:33:52 +0100

    New componennts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './views/about-us/about-us.component';
 import { BuyComponent } from './views/buy/buy.component';
+import { FormComponent } from './views/form/form.component';
 import { MainComponent } from './views/main/main.component';
 import { TermsComponent } from './views/terms/terms.component';
  
@@ -11,6 +12,7 @@ const routes: Routes = [
    { path: 'about-us', component: AboutUsComponent },
    { path: 'terms', component: TermsComponent },
    { path: 'buy', component: BuyComponent },
+   { path: 'form', component: FormComponent }
 
 
 ];
